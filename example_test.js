f50ae0a4c17d00079ec99b0fabad9ddae2df18d7
Aniko Sallai <aniko.sallai@gmail.com>
2023-12-10 20:36:25 +0100

    97. Assertions

--- example_test.js
+++ example_test.js
@@ -3,4 +3,7 @@ Feature('example');
 Scenario('Should load Example.com website',  ({ I }) => {
     I.amOnPage('https://example.com')
     I.see('Example')
+    I.dontSee('Google')
+    I.seeElement('h1')
+    I.dontSeeElement('#dontexist')
 });
