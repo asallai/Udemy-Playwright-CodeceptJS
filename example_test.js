dc7bf8b408a7e56019605528c120c74e95514db4
Aniko Sallai <aniko.sallai@gmail.com>
2023-12-10 20:39:01 +0100

    98. Multiple Scenarios

--- example_test.js
+++ example_test.js
@@ -1,6 +1,22 @@
-Feature('example');
+Feature('My test suite');
 
 Scenario('Should load Example.com website',  ({ I }) => {
+    I.amOnPage('https://example.com')
+    I.see('Example')
+    I.dontSee('Google')
+    I.seeElement('h1')
+    I.dontSeeElement('#dontexist')
+});
+
+Scenario('Should load Example.com website - 2nd test',  ({ I }) => {
+    I.amOnPage('https://example.com')
+    I.see('Example')
+    I.dontSee('Google')
+    I.seeElement('h1')
+    I.dontSeeElement('#dontexist')
+});
+
+Scenario('Should load Example.com website - 3rd test',  ({ I }) => {
     I.amOnPage('https://example.com')
     I.see('Example')
     I.dontSee('Google')
