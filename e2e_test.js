97e8668f58ef6b05a9f9e09222709cf7d7e31ced
Aniko Sallai <aniko.sallai@gmail.com>
2023-12-14 22:40:17 +0100

    102. Page Objects Model

--- e2e_test.js
+++ e2e_test.js
@@ -9,12 +9,12 @@ After(({ I }) => {
     console.log("AFTER HOOK")
 })
 
-Scenario("Login Test - Negative", ({ I }) => {
+Scenario("Login Test - Negative", ({ I, LoginPage }) => {
     I.click("#signin_button")
     I.seeElement("#login_form")
 
-    I.fillField("#user_login", "invalid username")
-    I.fillField("#user_password", "invalid password")
-    I.click(".btn-primary")
+    LoginPage.submitLogin("invalid username", "invalid password")
+    LoginPage.assertLoginFormIsVisible()
+
     I.seeElement(".alert-error")
 })
